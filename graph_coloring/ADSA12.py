40656d314a1555b89f5ff52e6910a06914e98cf6
romanponomarew <55201015+romanponomarew@users.noreply.github.com>
2021-01-26 16:15:54 +0300

    Add files via upload

---
+++ graph_coloring/ADSA12.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python
+
+"""файл для асинхронного моделирования с имзмененными исходными данными(много цветов)"""
+"""Другой подсчет штрафа в цикле"""
+from random import choice
+import random
+import json
+import asyncio
+from time import time, sleep
+
+
+
+#Метод считывания графа из файла .json
+with open("data(color)4.json", 'r', encoding='utf-8') as fh: #открываем файл на чтение
+    graph = json.load(fh) #загружаем из файла данные в словарь graph
+print("GRAPH=", graph)
+
+resulting_var = 0
+class Agent():
+    """Класс для создания агента(каждого узла модели)"""
+    def __init__(self, name): #name = цифры
+        """Конструктор - изначальные аттрибуты агента при создании:"""
+        self.name = name
+        self.neighbors = []  # Список соседей агента
+        self.val = graph["vars"][self.name]["initial"]   # Определение изначального цвета агента(задан в исходных данных)
+        self.domen = graph["vars"][self.name]["domain"]  # Домен значений
+        self.current_cycle = {}  # Состояние текущего цикла агента
+        self.next_cycle = {}    # Состояние следующего цикла агента
+        self.current_cost  = 0   # Текущая стоимость(сумма штрафов) агента
+        self.res = 0   #Результат работы всех циклов агента
+        print(f"Агент {self.name} - создан")
+        print(f"Значение агента ({self.name}) = ", self.val)
+
+    def neighbor_search(self):
+        """Определение списка соседей для текущего агента по исходным данным:"""
+        for name in graph["pairs"]:
+            if self.name == name[0]:
+                self.neighbors.append(name[1])
+            elif self.name == name[1]:
+                self.neighbors.append(name[0])
+        self.neighbors = list(set(self.neighbors))  # Оставляем только уникальные значения в списке
+        print(f"Соседи агента {self.name} = ", self.neighbors)
+        print()
+
+    async def send(self, agent):
+        """Запись значения текущего агента переданному соседу(запись в его состояния циклов)"""
+        if self.name in dict1[agent].neighbors: # Выполняем функцию только если соседи:
+            print("------------")
+            print(f"Агент {self.name} - сосед агента {agent}")
+            print("Выполняем метод send")
+            if self.name not in dict1[agent].current_cycle: #Есть ли значение текущего агента в текущем цикле соседа(i)
+                print(f"Текущий Агент({self.name}) еще не отсылал свое значение агенту({dict1[agent].name})")
+                dict1[agent].current_cycle[self.name] = self.val #Запись в словарь с ключом - имя текущего агента
+                print(f"Текущий цикл агента({dict1[agent].name}) равен", dict1[agent].current_cycle) #Состояние текущего цикла соседа(i)
+                if len(dict1[agent].neighbors) == len(dict1[agent].current_cycle): #Проверка на законченность цикла соседа
+                    print(f"Цикл агента ({dict1[agent].name}) - завершен!")
+                    # Расчет лучшего значения для переменной из цикла:
+                    await dict1[agent].evaluate_cycle(agent)
+
+            else:   #Если текущий агент уже есть в текущем цикле соседа:
+                print(f"Текущий Агент{self.name} уже отправлял значение агенту ({dict1[agent].name})")
+                dict1[agent].next_cycle[self.name] = self.val #Запись значения текущего агента в следующий цикл соседа
+                print(f"Поэтому записано в следующий цикла агента({dict1[agent].name}) = ", dict1[agent].next_cycle)
+            print("------------")
+            print()
+
+    async def func(self, val1, val2, name_neighbor):
+        """Метод расчета стоимости между двумя значениями переменных агентов"""
+        #global res1
+        print("--------------")
+        print(f"Выполняем метод func между ({self.name}) и ({name_neighbor})")
+        res2 = 0
+        for function in graph["functions"]:
+            if (val1 == function[0] and val2 == function[1]) or (val1 == function[1] and val2 == function[2]):
+                res2 = function[2]
+        print(f"Значения текущего агента ({self.name})=({val1}), а значение агента ({name_neighbor})=({val2})")
+        print(f"Поэтому штраф равен ", res2)
+        print("--------------")
+        print()
+        return res2
+
+    async def evaluate_cycle(self, name):
+        """Метод выбора наилучшего значения для переменной агента исходя из минимальной стоимости(выполняется в конце цикла)"""
+        #dict1[name].current_cycle[dict1[name].name] = dict1[name].val
+        print(f"Текущий цикл агента ({dict1[name].name}) = ", dict1[name].current_cycle)
+        print("----------------")
+        print(f"Выполняем метод расчета стоимости агента {name}")
+
+        cost_otnosit = {}
+        cost_current = 0
+        for key, value in dict1[name].current_cycle.items():
+            # Расчет стоимости агентом при текущем значении, суммируя стоимости со всеми соседями:
+            print("Расчет стоимости при текущем значении")
+            cost_current = cost_current + await dict1[name].func(self.val, value, key)
+        cost_otnosit[dict1[name].val] = cost_current
+        print(f"Стоимость агента() при текущем значении ({dict1[name].val}) равна", cost_otnosit)
+
+        # Создание словаря со стоимостью относительно состояния соседей, подставляя все значения из домена и суммируя их
+        for args in dict1[name].domen:
+            res1 = 0
+            for key, value in dict1[name].current_cycle.items():
+                res1 = res1 + await dict1[name].func(args, value, key)
+                cost_otnosit[args] = res1
+            print("cost_otnosit=", cost_otnosit)
+
+        # Выбор значения для переменной агента исходя из стоимостей
+        val_min = 0
+        for key, value in cost_otnosit.items():
+            if value == min(cost_otnosit.values()):
+                print(f"Минимальная стоимость агентом({dict1[name].name}) достигается при цвете", key)
+                print(f"Значение агента({dict1[name].name}) меняем на", key)
+                val_min = value
+                dict1[name].val = key
+                print(f"Текущий цвет агента({dict1[name].name})=", dict1[name].val)
+                dict1[name].res = dict1[name].res + value # Подсчет общей стоимости агента за всю работу модели
+        global resulting_var
+        resulting_var = resulting_var + val_min
+        print(f"Прибавляем к финальной стоимости штраф цикла агента({dict1[name].name}) = ", val_min)
+        print("Финальная стоимость на данный момент равна = ", resulting_var)
+        cost_otnosit = {}
+        dict1[name].current_cycle = {}  #Обнуляем текущий цикл:
+        print("----------------")
+        print()
+
+def results():
+    """Подсчет общей суммы штрафов всех агентов"""
+    summa = 0
+    for agent in graph["vars"].keys():
+        summa = summa + dict1[agent].res
+    for name in graph["vars"].keys():
+        print(f"Цвет агента ({dict1[name].name}) - {dict1[name].val}")
+    print("----------------------------------------------------")
+    print("Общая стоимость модели(сумма всех штрафов) равна " + str(summa))
+    print("Общий штраф по новому расчету равен ", resulting_var)
+
+
+def init_agents():
+    """Создание агентов по ключам графа"""
+    global dict1
+    for i in graph["vars"].keys():
+        dict1[i] = Agent(i)
+        dict1[i].neighbor_search()
+
+
+
+#=======================MAIN==================#
+
+start = time()
+
+#Создание агентов по ключам графа
+dict1 = {}
+init_agents()
+
+#agent1 = random.choice(list(graph["vars"].keys())) # Случайный агент из всех возможных
+
+tasks = []
+c = 0
+event_loop = asyncio.get_event_loop()
+for i in range(0, 1500):
+    for agent in graph["vars"].keys():
+        tasks.append(event_loop.create_task(dict1[agent].send(random.choice(list(graph["vars"].keys())))))
+        c = c +1
+        #print("Действие номер - ", c)
+print("Длина очереди асинхронных действий=", len(tasks))
+wait_tasks = asyncio.wait(tasks)
+event_loop.run_until_complete(wait_tasks)
+event_loop.close()
+
+results()
+
+#print(list(dict1.keys()))
+
+
+
+#########Варианты для обычных тестов##########
+# c = 0
+# c1 = 10
+# for i in range(1500):
+#     for agent in graph["vars"].keys():
+#         #c1 = c1 - 1
+#         dict1[agent].send(random.choice(list(graph["vars"].keys())))
+#         c = c +1
+#
+# results()
+
+#print(choice(list(dict1.keys())))
+
+print("Кол-во действий = ", c)
+print(f"Script work time is {time() - start} s.")
+
+
+
